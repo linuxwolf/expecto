21df18b18bdfe27c2b45e4ea95b1b4690ee10982
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-01-14 12:15:04 -0700

    flags tracking implementation

---
+++ src/flags.ts
@@ -0,0 +1,83 @@
+/**
+ * @file Manages flags on Expect wrappers.
+ *
+ * @copyright 2023 Matthew A. Miller
+ */
+
+/**
+ * Flag for negating a check.
+ */
+export const NOT = "not";
+/**
+ * Flag for deep comparisons in a check.
+ */
+export const DEEP = "deep";
+
+/** Manages the flags on a specific owning object. */
+export class Flags {
+  #values: Record<string, boolean> = {};
+
+  constructor(copy?: Flags) {
+    if (copy) {
+      this.#values = {
+        ...copy.#values,
+      };
+    }
+  }
+
+  all(): string[] {
+    return Object.entries(this.#values)
+      .filter(([_, v]) => !!v)
+      .map(([k, _]) => k)
+      .sort();
+  }
+
+  /**
+   * Retrieve the value for the given flag.  This method `false` if not set.
+   *
+   * @param name The named flag.
+   * @returns `true` if the flag is set; `false` otherwise.
+   */
+  get(name: string): boolean {
+    return this.#values[name] || false;
+  }
+  /**
+   * Sets the given flag.
+   *
+   * @param name The named flag.
+   */
+  set(name: string) {
+    this.#values[name] = true;
+  }
+  /**
+   * Unsets the given flag.
+   *
+   * @param name The named flag.
+   */
+  unset(name: string) {
+    delete this.#values[name];
+  }
+  /**
+   * Toggles the given flag.  If it was previously `false` or unset, now it is
+   * `true`; if it was `true`, not it is `false`.
+   *
+   * @param name The named flag
+   * @returns The current value
+   */
+  toggle(name: string): boolean {
+    const value = !this.get(name);
+    if (value) {
+      this.set(name);
+    } else {
+      this.unset(name);
+    }
+    return value;
+  }
+
+  /**
+   * Clears all of the set flags.
+   */
+  clear() {
+    this.#values = {};
+  }
+}
---
+++ test/flags.test.ts
@@ -0,0 +1,66 @@
+import { assert, equal } from "std/testing/asserts.ts";
+import { beforeEach, describe, it } from "std/testing/bdd.ts";
+
+import { Flags } from "../src/flags.ts";
+
+describe("flags", () => {
+  describe("class Flags", () => {
+    describe("ctor", () => {
+      it("constructs an empty Flags", () => {
+        const result = new Flags();
+        assert(equal(result.all(), []));
+      });
+      it("constructs a copy", () => {
+        const flags = new Flags();
+        flags.set("foo");
+        flags.set("bar");
+        flags.set("baz");
+
+        const result = new Flags(flags);
+        assert(equal(result.all(), flags.all()));
+      });
+    });
+    describe("values", () => {
+      let flags: Flags;
+
+      beforeEach(() => {
+        flags = new Flags();
+      });
+
+      it("get()s / set()s / unset()s a flag", () => {
+        assert(!flags.get("foo"));
+        flags.set("foo");
+        assert(flags.get("foo"));
+        flags.unset("foo");
+        assert(!flags.get("foo"));
+        flags.set("foo");
+        assert(flags.get("foo"));
+      });
+      it("toggle()s a flag", () => {
+        assert(!flags.get("foo"));
+
+        assert(flags.toggle("foo"));
+        assert(flags.get("foo"));
+
+        assert(!flags.toggle("foo"));
+        assert(!flags.get("foo"));
+      });
+
+      it("clear()s all values", () => {
+        const flags = new Flags();
+        flags.set("foo");
+        flags.set("deep");
+        flags.set("negate");
+        assert(equal(flags.all(), ["deep", "foo", "negate"]));
+        assert(flags.get("foo"));
+        assert(flags.get("deep"));
+        assert(flags.get("negate"));
+
+        flags.clear();
+        assert(!flags.get("foo"));
+        assert(!flags.get("deep"));
+        assert(!flags.get("negate"));
+      });
+    });
+  });
+});

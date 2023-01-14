40fa5876d99e9f6416382bc78d0db2ec310a3920
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-01-14 14:16:06 -0700

    mixin registry implementation

---
+++ src/registry.ts
@@ -0,0 +1,43 @@
+/**
+ * @file Defines a registry for Expecto mixins.
+ * 
+ * @copyright 2023 Matthew A. Miller
+ */
+
+import { ExpectoBase, ExpectoConstructor } from "./base.ts";
+
+/** Function to register a new Expecto mixin */
+export type RegisterFunction<T> = (Base: ExpectoConstructor<T>) => ExpectoConstructor<T>;
+
+/**
+ * Manages the Expecot wrapper mixins and creating instances with all applied
+ * mixins.
+ */
+export class Registry<T> {
+  #Current: ExpectoConstructor<T> = ExpectoBase;
+
+  /**
+   * The Expecto wrapper type, with all mixins applied.
+   */
+  get type(): typeof ExpectoBase<T> { return this.#Current; }
+
+  /**
+   * Applies a new mixin within this Registry.
+   * 
+   * @param fn The mixin register to apply.
+   */
+  apply(fn: RegisterFunction<T>) {
+    this.#Current = fn(this.#Current);
+  }
+
+  /**
+   * Creates a new Expecto wrapper over `target`.  This method applies all
+   * registered mixins to `ExpectoBase`.
+   * 
+   * @param target The value under test
+   * @returns The Expecto wrapper over `target`
+   */
+  create(target: T): ExpectoBase<T> {
+    return new this.#Current(target);
+  }
+}
---
+++ test/registry.test.ts
@@ -0,0 +1,41 @@
+/**
+ * @copyright 2023 Matthew A. Miller
+ */
+
+import { assert } from "std/testing/asserts.ts";
+import { beforeEach, describe, it } from "std/testing/bdd.ts";
+import { ExpectoBase, ExpectoConstructor } from "../src/base.ts";
+
+import { Registry } from "../src/registry.ts";
+
+describe("registry", () => {
+  const target = new Date();
+
+  describe("class Registry", () => {
+    let registry: Registry<typeof target>;
+    beforeEach(() => {
+      registry = new Registry();
+    });
+  
+    it("returns a generic ExpectoBase by default", () => {
+      assert(registry.type === ExpectoBase);
+
+      const result = registry.create(target);
+      assert(result instanceof ExpectoBase);
+    });
+    it("applies a mixin and creates an instance with it", () => {
+      function dummy<T>(Base: ExpectoConstructor<T>): ExpectoConstructor<T> {
+        return class DummyExpecto<T> extends Base {
+          dummy(): DummyExpecto<T> { return this; }
+        }
+      }
+
+      let result = registry.create(target);
+      assert(!("dummy" in result));
+
+      registry.apply(dummy);
+      result = registry.create(target);
+      assert("dummy" in result);
+    });
+  });
+});

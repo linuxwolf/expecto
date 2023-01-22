e72646849ae818239c1a03321cf118572b875675
Matthew A. Miller <linuxwolf@outer-planes.net>
2023-01-22 09:11:02 -0700

    export public interfaces

---
+++ mod/index.ts
@@ -0,0 +1,5 @@
+/**
+ * @file Exports for standard usage
+ */
+
+export { expect, use } from "../src/main.ts";
---
+++ mod/mixin.ts
@@ -0,0 +1,11 @@
+/**
+ * @file Exports for developing custom Mixins
+ * 
+ * @copyright 2023 Matthew A. Miller
+ */
+
+import { ExpectoBase } from "../src/base.ts";
+import { MixinConstructor } from "../src/mixin.ts";
+
+export { ExpectoBase };
+export type { MixinConstructor };
